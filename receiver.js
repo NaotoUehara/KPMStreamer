9e757dbc0981107a0fce8f3e47e9cbab99570354
NaotoUehara <88977917+NaotoUehara@users.noreply.github.com>
2021-09-16 23:52:29 +0900

    Add files via upload

--- receiver.js
+++ receiver.js
@@ -1,16 +1,35 @@
 const peer = new Peer({key: '8bcc94dc-592d-4a96-bb15-1c80f2066616'});
-
+var fileToPlay = null;
+peer.on("open", function (){document.getElementById('idview').value = peer.id;});
 function main() 
 {
-    document.getElementById('connect').onclick = () => {
-        console.log('///')
-        const idToConnect = document.getElementById('id').value;
-        const mediaConnection = peer.call(idToConnect);
-        mediaConnection.on('stream', stream => {
+   // const peer = new Peer({key: '8bcc94dc-592d-4a96-bb15-1c80f2066616'});
+
+    var fileInput = document.getElementById('filename')
+    fileInput.addEventListener( "change", function (event) {
+        var URL = URL || webkitURL;
+        var file = event.target.files[0];
+        //document.querySelector('video').src = URL.createObjectURL(file);
+        fileToPlay = URL.createObjectURL(file);
+        //document.getElementById('filename').innerHTML = document.querySelector('video').src;
+    }, false );
+
+    peer.on("connection",function(connection) {
+        console.log("connection");
+        connection.on("data", msg => {
+            console.log("aaa");
             const videoElm = document.getElementById('video')
-            videoElm.srcObject = stream;
-            videoElm.play();
-            videoElm.webkitRequestFullscreen();
+            if (msg == "play")
+            {   
+                videoElm.src = fileToPlay;
+                videoElm.play();
+                videoElm.webkitRequestFullscreen();
+            }
+            else
+            {
+                videoElm.pause();
+            }
         });
-    }
+    })
+
 }
--- sender.js
+++ sender.js
@@ -15,12 +15,31 @@ function capture(peerObj) {
 
 function main()
 {   
-    document.getElementById('share').onclick = share;
+    document.getElementById('register').onclick = register;
+    document.getElementById('play').onclick = play;
 }
 
-function share()
+
+function register()
 {
-  const peer = new Peer({key: '8bcc94dc-592d-4a96-bb15-1c80f2066616'});
+  console.log('register')
+    var id = document.getElementById('id').value;
+    const peer = new Peer({key: '8bcc94dc-592d-4a96-bb15-1c80f2066616'});
+    peer.on("open",function() {
+      peerConnections.push(peer.connect(id));
+      //peerConnections.push(connection)
+    });
+}
+
+function play()
+{
+  console.log("play")
+  for (var elm of peerConnections)
+  {
+    elm.send("play");
+    console.log("play pls")
+  }
+  /*
   var insert_before = document.getElementById('end');
   var id_view = document.createElement("p");
   insert_before.before(id_view);
@@ -28,7 +47,5 @@ function share()
   peer.on('open', () => {
     id_view.textContent = peer.id;
   });
-  peer.on('call', mediaConnection => {
-    mediaConnection.answer(localStreams[peer]);
-  });
+  */
 }
