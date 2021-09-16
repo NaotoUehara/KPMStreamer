e0a3411315803aabb069f4cab755a013a877e28e
NaotoUehara <88977917+NaotoUehara@users.noreply.github.com>
2021-09-17 00:10:38 +0900

    Add files via upload

--- receiver.js
+++ receiver.js
@@ -1,6 +1,9 @@
 const peer = new Peer({key: '8bcc94dc-592d-4a96-bb15-1c80f2066616'});
 var fileToPlay = null;
 peer.on("open", function (){document.getElementById('idview').value = peer.id;});
+
+const videoElm = document.getElementById('video')
+
 function main() 
 {
    // const peer = new Peer({key: '8bcc94dc-592d-4a96-bb15-1c80f2066616'});
@@ -11,6 +14,7 @@ function main()
         var file = event.target.files[0];
         //document.querySelector('video').src = URL.createObjectURL(file);
         fileToPlay = URL.createObjectURL(file);
+        videoElm.webkitRequestFullscreen();
         //document.getElementById('filename').innerHTML = document.querySelector('video').src;
     }, false );
 
